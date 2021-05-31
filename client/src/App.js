c94726ca28687183afd624fed398e15efd9988b9
h-shahid3 <hshassan3@hotmail.com>
2021-05-31 13:54:10 +0500

    almost complete

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import React, {useState, useCallback, useEffect} from 'react';
 import HanziWriter from 'hanzi-writer'
 
 const App = () =>{
-  const [inputChar,setInputChar] = useState('爱');
+  const [inputString,setInputString] = useState('爱');
   let [defs,setDefs] = useState(()=>{
     let tmp = JSON.stringify({
       definition:"to love/affection/to be fond of/to like",
@@ -15,9 +15,37 @@ const App = () =>{
     let defObj = JSON.parse(tmp);
     return [defObj];
   });//Definition and Description
+  let [sDefinition,setSDefinition] = useState(()=>{
+    return defs[0].definition;
+  })
+  let [sPinyin,setSPinyin] = useState(()=>{
+    return defs[0].pinyin;
+  })
+  let [sTraditional,setSTraditional] = useState(()=>{
+    return defs[0].traditional;
+  })
+  let [sSimplified,setSSimplified] = useState(()=>{
+    return defs[0].simplified;
+  })
+
+  /*useEffect(()=>{
+    setSDefinition(()=>{
+      return defs[0].definition;
+    });
+    setSPinyin(()=>{
+      return defs[0].pinyin;
+    });
+    setSTraditional(()=>{
+      return defs[0].traditional;
+    })
+    setSSimplified(()=>{
+      return defs[0].simplified;
+    })
+  },[defs])*/
+
   useEffect(()=>{
     console.log("In UseEffect");
-    let writer = HanziWriter.create('character-target-div', inputChar, {
+    let writer = HanziWriter.create('character-target-div', inputString, {
       width: 500,
       height: 500,
       padding: 5,
@@ -28,7 +56,7 @@ const App = () =>{
   //[{definition:"",pinyin:"",Simplified:"",Traditional:""}]
   const formSubmitted = async (event) =>{
     event.preventDefault();
-    let definitions = await callAPI(inputChar);
+    let definitions = await callAPI(inputString);
     definitions.forEach((item,index)=>{
       definitions[index] = JSON.parse(JSON.stringify(definitions[index]));
     })
@@ -38,26 +66,44 @@ const App = () =>{
       return;
     }
     setDefs(()=>{
-      defs = definitions.slice()
+      defs = [];
+      definitions.forEach((item,index)=>{
+        defs.push(JSON.parse(JSON.stringify(item)));
+      })
+      console.log(defs);
+      setSDefinition(()=>{
+        return defs[0].definition;
+      });
+      setSPinyin(()=>{
+        return defs[0].pinyin;
+      });
+      setSTraditional(()=>{
+        return defs[0].traditional;
+      })
+      setSSimplified(()=>{
+        return defs[0].simplified;
+      })
     })
     document.getElementById('character-target-div').innerHTML=''; // Causes the div to be empty to prepare for new character
-    let writer = HanziWriter.create('character-target-div', inputChar, {
-      onLoadCharDataSuccess: function(data) {
-        console.log('Successfully found the character!');
-      },
-      onLoadCharDataError: function(reason) {
-        console.log('Oh No! Something went wrong when finding the character! :(');
-        window.alert("The Input Character is invalid. Please input a single character only in either Simplified or Traditional Chinese.");
-      },
-      width: 500,
-      height: 500,
-      padding: 5,
-      delayBetweenStrokes: 500
-    });
-    writer.loopCharacterAnimation();
+    for (let i = 0; i < inputString.length; i++) {
+      let singleChar = inputString.charAt(i);
+      HanziWriter.create('character-target-div', singleChar, {
+        onLoadCharDataSuccess: function(data) {
+          console.log('Successfully found the character!');
+        },
+        onLoadCharDataError: function(reason) {
+          console.log('Oh No! Something went wrong when finding the character! :(');
+          window.alert("The Input Character is invalid. Please input a single character only in either Simplified or Traditional Chinese.");
+        },
+        width: 500,
+        height: 500,
+        padding: 5,
+        delayBetweenStrokes: 500
+      }).loopCharacterAnimation();
+    }
   }
   const handleInputCharChange = useCallback((event) =>{
-    setInputChar(event.target.value);
+    setInputString(event.target.value);
   },[]);
 
   return(
@@ -80,6 +126,12 @@ const App = () =>{
       </nav>
       <div className="row">
         <div className="col-sm-8 container p-3 my-3 border">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Definition: {sDefinition}</li>
+            <li className="list-group-item">Pinyin: {sPinyin}</li>
+            <li className="list-group-item">Simplified: {sSimplified}</li>
+            <li className="list-group-item">Traditional: {sTraditional}</li>
+          </ul>
         </div>
         <div className="col-sm-4 container p-3 my-3 border" id="character-target-div"></div>
       </div>
