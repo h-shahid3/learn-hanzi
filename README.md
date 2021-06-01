7e29d10ccfd79bc8eb28ea15e228197b171c1f08
h-shahid3 <hshassan3@hotmail.com>
2021-06-01 10:16:31 +0500

    finishing up

---
+++ README.md
@@ -0,0 +1,32 @@
+## Table of contents
+* [Introduction](#introduction)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Final Result](#final-result)
+
+## Introduction
+This project is a website which facilitates students of the Chinese langauge. The user inputs a character or word, upon which the definition and properties of the input string are queried and displayed on the webpage. The stroke order is also displayed and animated to facilitate the student in learning to write the character.
+	
+## Technologies
+This website is created using:
+* ReactJS version: 17.0.2
+* Bootstrap version: 4.5.2
+* NodeJS version: 12.16.3
+* npm packages: express, cors, nodemon, hanzi, helmet, morgan
+* eslint version: 7.26.0
+	
+## Setup
+To run this project, first clone the repo. Navigate to both the client and server folders and run npm start to host the website locally.
+```
+$ cd learn-hanzi
+$ cd server
+$ npm run start
+```
+```
+$ cd learn-hanzi
+$ cd client
+$ npm run start
+```
+## Final Result
+Below you can see a recorded gif demonstrating the website's capabilities and design. Happy learning!
+![screen-gif](./ai-an-display.gif)
--- client/src/App.js
+++ client/src/App.js
@@ -15,36 +15,8 @@ const App = () =>{
     let defObj = JSON.parse(tmp);
     return [defObj];
   });//Definition and Description
-  let [sDefinition,setSDefinition] = useState(()=>{
-    return defs[0].definition;
-  })
-  let [sPinyin,setSPinyin] = useState(()=>{
-    return defs[0].pinyin;
-  })
-  let [sTraditional,setSTraditional] = useState(()=>{
-    return defs[0].traditional;
-  })
-  let [sSimplified,setSSimplified] = useState(()=>{
-    return defs[0].simplified;
-  })
-
-  /*useEffect(()=>{
-    setSDefinition(()=>{
-      return defs[0].definition;
-    });
-    setSPinyin(()=>{
-      return defs[0].pinyin;
-    });
-    setSTraditional(()=>{
-      return defs[0].traditional;
-    })
-    setSSimplified(()=>{
-      return defs[0].simplified;
-    })
-  },[defs])*/
 
   useEffect(()=>{
-    console.log("In UseEffect");
     let writer = HanziWriter.create('character-target-div', inputString, {
       width: 500,
       height: 500,
@@ -61,42 +33,30 @@ const App = () =>{
       definitions[index] = JSON.parse(JSON.stringify(definitions[index]));
     })
     if(definitions[0].error){
-      console.log("caught");
-      window.alert("The Input Character is invalid. Please input a single character only in either Simplified or Traditional Chinese.");
+      window.alert("The Input Character is invalid. Please input characters only in either Simplified or Traditional Chinese.");
       return;
     }
     setDefs(()=>{
-      defs = [];
+      let tmpDef = [];
       definitions.forEach((item,index)=>{
-        defs.push(JSON.parse(JSON.stringify(item)));
+        tmpDef.push(JSON.parse(JSON.stringify(item)));
       })
-      console.log(defs);
-      setSDefinition(()=>{
-        return defs[0].definition;
-      });
-      setSPinyin(()=>{
-        return defs[0].pinyin;
-      });
-      setSTraditional(()=>{
-        return defs[0].traditional;
-      })
-      setSSimplified(()=>{
-        return defs[0].simplified;
-      })
+      return tmpDef;
     })
     document.getElementById('character-target-div').innerHTML=''; // Causes the div to be empty to prepare for new character
     for (let i = 0; i < inputString.length; i++) {
       let singleChar = inputString.charAt(i);
+      let iStrLength = inputString.length;
       HanziWriter.create('character-target-div', singleChar, {
         onLoadCharDataSuccess: function(data) {
           console.log('Successfully found the character!');
         },
         onLoadCharDataError: function(reason) {
           console.log('Oh No! Something went wrong when finding the character! :(');
-          window.alert("The Input Character is invalid. Please input a single character only in either Simplified or Traditional Chinese.");
+          window.alert("The Input Character is invalid. Please input characters only in either Simplified or Traditional Chinese.");
         },
-        width: 500,
-        height: 500,
+        width: 500/iStrLength,
+        height: 500/iStrLength,
         padding: 5,
         delayBetweenStrokes: 500
       }).loopCharacterAnimation();
@@ -106,8 +66,32 @@ const App = () =>{
     setInputString(event.target.value);
   },[]);
 
+  function RenderDefsandStuff(props){
+    if(typeof defs == 'undefined'){
+      return null;
+    }
+    else
+      {
+        return(
+          <ol>
+          {defs.map((item,index)=>{
+            return(
+            <li key={index}>
+              <ul>
+                <li key={item.definition} className="list-group-item">Definition: {item.definition}</li>
+                <li key={item.definition+1} className="list-group-item">Pinyin: {item.pinyin}</li>
+                <li key={item.definition+2} className="list-group-item">Simplified: {item.simplified}</li>
+                <li key={item.definition+3} className="list-group-item">Traditional: {item.traditional}</li>
+              </ul>
+            </li>
+            )
+          })}
+          </ol>
+        )}
+  }
+
   return(
-    <div>
+    <div >
       <nav className="navbar navbar-expand-sm bg-light navbar-dark justify-content-center">
         <img className="navbar-brand" src={logo} alt ="logo"></img>
         <div className="form-inline">
@@ -126,19 +110,13 @@ const App = () =>{
       </nav>
       <div className="row">
         <div className="col-sm-8 container p-3 my-3 border">
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Definition: {sDefinition}</li>
-            <li className="list-group-item">Pinyin: {sPinyin}</li>
-            <li className="list-group-item">Simplified: {sSimplified}</li>
-            <li className="list-group-item">Traditional: {sTraditional}</li>
-          </ul>
+          <h5 className="lead" id="meaning">Meanings</h5>
+          <RenderDefsandStuff/>
         </div>
         <div className="col-sm-4 container p-3 my-3 border" id="character-target-div"></div>
       </div>
     </div>
   );
 };
-
-
 export default App;
 
