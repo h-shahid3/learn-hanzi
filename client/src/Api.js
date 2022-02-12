5b2440588b5ced0da6206e334ce77772e528a101
h-shahid3 <hassans@sfu.ca>
2022-02-11 20:40:46 -0800

    readying

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,8 +1,8 @@
-const API_URL = 'http://localhost:1337'
+//const API_URL = 'http://localhost:1337'
 
 
 
 export async function callAPI(inputChar){
-    const response = await fetch(`${API_URL}/word?char=${inputChar}`);
+    const response = await fetch(`/word?char=${inputChar}`);
     return response.json();
 }
